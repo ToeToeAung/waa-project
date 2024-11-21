8ad25e2169197b07472033903685c2557e2b2bc9
sarun tapee <tpzrun@gmail.com>
2024-11-21 08:08:34 -0600

    products

--- frontend/src/components/ProductDetails.js
+++ frontend/src/components/ProductDetails.js
@@ -8,43 +8,80 @@ import {
   Typography,
   TextField,
 } from "@mui/material"
-import React, { useEffect, useState } from "react"
+import React, { useCallback, useEffect, useReducer, useState } from "react"
 import { useParams } from "react-router-dom"
+import { getProductById, getReviews } from "../api/public"
+import { addReview } from "../api/buyer"
+import { useAlert } from "../hook/alert"
+import { ERR_INCORRECT_CREDENTIAL, ERR_UNKNOWN } from "../entity/error"
+import { useSelector } from "react-redux"
+import { USER_ROLE_ADMIN, USER_ROLE_BUYER } from "../entity/Auth"
+import { deleteReview } from "../api/admin"
 
-const getProduct = (id) => {
-  return Promise.resolve({
-    id,
-    name: "product 123",
-    category: "category-1",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    quantity: 20,
-    price: 120,
-    sellerId: 1,
-    rating: 3.5,
-  })
-}
+// const getProduct = (id) => {
+//   return Promise.resolve({
+//     id,
+//     name: "product 123",
+//     category: "category-1",
+//     description:
+//       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+//     quantity: 20,
+//     price: 120,
+//     sellerId: 1,
+//     rating: 3.5,
+//   })
+// }
 
-const getReviews = (productId) => {
-  return Promise.resolve([
-    { id: 1, content: "good", rating: 4.5 },
-    { id: 1, content: "bad", rating: 2.5 },
-    { id: 1, content: "very good", rating: 5 },
-  ])
+// const getReviews = (productId) => {
+//   return Promise.resolve([
+//     { id: 1, content: "good", rating: 4.5 },
+//     { id: 1, content: "bad", rating: 2.5 },
+//     { id: 1, content: "very good", rating: 5 },
+//   ])
+// }
+
+const SET_CONTENT = "set_content"
+const SET_RATING = "set_rating"
+const CLEAR_FORM = "clear_form"
+
+const emptyForm = {
+  content: "",
+  rating: "",
 }
 
 export function ProductDetails() {
   const { id } = useParams()
+  const me = useSelector((state) => state.auth.me)
+  const alert = useAlert()
   const [product, setProduct] = useState()
   const [reviews, setReviews] = useState()
+  const [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case SET_CONTENT:
+        return { ...state, content: action.content }
+      case SET_RATING:
+        return { ...state, rating: action.rating }
+      case CLEAR_FORM:
+        return { ...emptyForm }
+      default:
+        return state
+    }
+  }, emptyForm)
 
   useEffect(() => {
-    getProduct(id).then((res) => setProduct(res))
+    getProductById(id).then((res) =>
+      // TODO: fix api
+      setProduct({ ...res, name: "product name" }),
+    )
+  }, [id])
+
+  const syncReview = useCallback(() => {
+    getReviews({ productId: id }).then((res) => setReviews(res))
   }, [id])
 
   useEffect(() => {
-    getReviews(id).then((res) => setReviews(res))
-  }, [id])
+    syncReview()
+  }, [syncReview])
 
   return (
     <Box>
@@ -54,11 +91,15 @@ export function ProductDetails() {
         <>
           <Typography variant="h5">{product.name}</Typography>
           <Typography variant="h6">Category</Typography>
-          <Typography variant="body2">{product.category}</Typography>
+          <Typography variant="body2">{product.category.name}</Typography>
           <Typography variant="h6">Rating</Typography>
           <Rating readOnly value={product.rating} precision={0.5} />
           <Divider sx={{ my: 2 }} />
           <Typography variant="body1">{product.description}</Typography>
+        </>
+      )}
+      {me?.role === USER_ROLE_BUYER && (
+        <>
           <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
             <Button variant="contained">Add to cart</Button>
             <TextField
@@ -69,15 +110,45 @@ export function ProductDetails() {
               value={1}
             />
           </Box>
+          <Typography variant="h6" sx={{ mt: 4 }}>
+            Add review
+          </Typography>
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+            <Rating
+              precision={0.5}
+              value={state.rating}
+              onChange={(e, v) => dispatch({ type: SET_RATING, rating: v })}
+            />
+            <TextareaAutosize
+              minRows={5}
+              value={state.content}
+              onChange={(e) =>
+                dispatch({ type: SET_CONTENT, content: e.target.value })
+              }
+            />
+          </Box>
+          <Button
+            sx={{ mt: 1 }}
+            variant="outlined"
+            onClick={async () => {
+              try {
+                await addReview({
+                  productId: id,
+                  content: state.content,
+                  rating: state.rating,
+                })
+                alert({ msg: "review added", level: "success" })
+                dispatch({ type: CLEAR_FORM })
+                syncReview()
+              } catch (e) {
+                alert({ msg: ERR_UNKNOWN, level: "error" })
+              }
+            }}
+          >
+            Submit
+          </Button>
         </>
       )}
-      <Typography variant="h6" sx={{ mt: 4 }}>
-        Add review
-      </Typography>
-      <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
-        <Rating precision={0.5} />
-        <TextareaAutosize minRows={5} />
-      </Box>
       {reviews && (
         <>
           <Typography variant="h6" sx={{ mt: 4 }}>
@@ -88,6 +159,24 @@ export function ProductDetails() {
               <Paper variant="outlined" sx={{ padding: 2 }}>
                 <Rating readOnly value={r.rating} precision={0.5} />
                 <Typography>{r.content}</Typography>
+                {me?.role === USER_ROLE_ADMIN && (
+                  <Button
+                    sx={{ mt: 2 }}
+                    color="error"
+                    variant="outlined"
+                    onClick={async () => {
+                      try {
+                        await deleteReview({ productId: id, reviewId: r.id })
+                        alert({ msg: "deleted review", level: "success" })
+                        syncReview()
+                      } catch (e) {
+                        alert({ msg: ERR_UNKNOWN, level: "error" })
+                      }
+                    }}
+                  >
+                    Delete
+                  </Button>
+                )}
               </Paper>
             ))}
           </Box>
--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -11,9 +11,11 @@ import {
   TextField,
   Typography,
 } from "@mui/material"
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useReducer, useState } from "react"
 import { Link } from "react-router-dom"
 import { Product } from "./Product"
+import { getCategories } from "../api/public"
+// import { getProducts } from "../api/public"
 
 const getProducts = ({ page, pageSize }) => {
   return Promise.resolve({
@@ -38,15 +40,24 @@ const PAGE_SIZE = 8
 
 export function Products() {
   const [productData, setProductData] = useState(null)
+  const [page, setPage] = useState(1)
   useEffect(() => {
-    getProducts({ page: 1, pageSize: PAGE_SIZE }).then((res) =>
-      setProductData(res),
+    getProducts({ page, pageSize: PAGE_SIZE }).then(
+      (res) => setProductData(res),
+      // setProductData({
+      //   products: res.map((r) => ({ ...r, rating: 2.5, name: "product name" })),
+      //   totalPage: 10,
+      // }),
     )
   }, [])
 
+  const onFilter = (filter) => {
+    console.log("here", filter)
+  }
+
   return (
     <Box sx={{ display: "flex", gap: 4, justifyContent: "space-between" }}>
-      <Filter />
+      <Filter onFilter={onFilter} />
       {!productData ? (
         "loading"
       ) : (
@@ -68,6 +79,8 @@ export function Products() {
             sx={{ marginTop: 2 }}
             count={productData.totalPage}
             color="primary"
+            page={page}
+            onChange={(e, v) => setPage(v)}
           />
         </Box>
       )}
@@ -75,14 +88,50 @@ export function Products() {
   )
 }
 
-const categories = [
-  { id: 1, name: "category-1" },
-  { id: 2, name: "category-2" },
-  { id: 3, name: "category-3" },
-]
+// const categories = [
+//   { id: 1, name: "category-1" },
+//   { id: 2, name: "category-2" },
+//   { id: 3, name: "category-3" },
+// ]
 
-function Filter() {
+const SET_CATEGORY_FILTER_ID = "set_category_filter_id"
+const SET_PRICE_FILTER_GT = "set_price_filter_gt"
+const SET_PRICE_FILTER_LT = "set_price_filter_lt"
+const SET_RATING_FILTER_GT = "set_rating_filter_gt"
+const SET_RATING_FILTER_LT = "set_rating_filter_lt"
+
+function Filter({ onFilter }) {
   const [rating, setRating] = useState([0, 5])
+  const [categories, setCategories] = useState([])
+  const [state, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case SET_CATEGORY_FILTER_ID:
+          return { ...state, categoryId: action.categoryId }
+        case SET_PRICE_FILTER_GT:
+          return { ...state, priceGt: action.priceGt }
+        case SET_PRICE_FILTER_LT:
+          return { ...state, priceLt: action.priceLt }
+        case SET_RATING_FILTER_GT:
+          return { ...state, ratingGt: action.ratingGt }
+        case SET_RATING_FILTER_LT:
+          return { ...state, ratingLt: action.ratingLt }
+        default:
+          return state
+      }
+    },
+    {
+      categoryId: "",
+      priceGt: "",
+      priceLt: "",
+      ratingGt: "",
+      ratingLt: "",
+    },
+  )
+
+  useEffect(() => {
+    getCategories().then((res) => setCategories(res))
+  }, [])
 
   return (
     <Box>
@@ -101,7 +150,17 @@ function Filter() {
         </Typography>
         <FormControl fullWidth>
           <InputLabel id="category">Category</InputLabel>
-          <Select label="Category" labelId="category">
+          <Select
+            label="Category"
+            labelId="category"
+            value={state.categoryId}
+            onChange={(e) => {
+              dispatch({
+                type: SET_CATEGORY_FILTER_ID,
+                categoryId: e.target.value,
+              })
+            }}
+          >
             <MenuItem>no filter</MenuItem>
             {categories.map((c) => (
               <MenuItem value={c.id}>{c.name}</MenuItem>
@@ -109,14 +168,49 @@ function Filter() {
           </Select>
         </FormControl>
         <Box sx={{ display: "flex", gap: 2 }}>
-          <TextField label="price >" />
-          <TextField label="price <" />
+          <TextField
+            type="number"
+            label="price >"
+            value={state.priceGt}
+            onChange={(e) =>
+              dispatch({ type: SET_PRICE_FILTER_GT, priceGt: e.target.value })
+            }
+          />
+          <TextField
+            type="number"
+            label="price <"
+            value={state.priceLt}
+            onChange={(e) =>
+              dispatch({ type: SET_PRICE_FILTER_LT, priceLt: e.target.value })
+            }
+          />
         </Box>
         <Box sx={{ display: "flex", gap: 2 }}>
-          <TextField label="rating >" />
-          <TextField label="rating <" />
+          <TextField
+            type="number"
+            label="rating >"
+            value={state.ratingGt}
+            onChange={(e) =>
+              dispatch({ type: SET_RATING_FILTER_GT, ratingGt: e.target.value })
+            }
+          />
+          <TextField
+            type="number"
+            label="rating <"
+            value={state.ratingLt}
+            onChange={(e) =>
+              dispatch({ type: SET_RATING_FILTER_LT, ratingLt: e.target.value })
+            }
+          />
         </Box>
-        <Button variant="contained">Apply</Button>
+        <Button
+          variant="contained"
+          onClick={() => {
+            onFilter(state)
+          }}
+        >
+          Apply
+        </Button>
       </Paper>
     </Box>
   )
