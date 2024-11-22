445c54747bce376d78c0d42e3dc5e8fadd666915
huazhou <hua.zhou@miu.edu>
2024-11-22 14:12:37 -0600

    add sql files for testing data

---
+++ sql/orders.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.orders (id, order_date, order_status, buyer_id) VALUES (1, '2024-11-22 11:56:39.891320', 'PENDING', 4);
+INSERT INTO public.orders (id, order_date, order_status, buyer_id) VALUES (2, '2024-11-22 11:58:13.318409', 'PENDING', 3);
+INSERT INTO public.orders (id, order_date, order_status, buyer_id) VALUES (3, '2024-11-22 12:02:01.793757', 'PENDING', 2);
+INSERT INTO public.orders (id, order_date, order_status, buyer_id) VALUES (4, '2024-11-22 12:02:48.737212', 'PENDING', 3);
+INSERT INTO public.orders (id, order_date, order_status, buyer_id) VALUES (5, '2024-11-22 12:12:02.672486', 'PENDING', 3);
+INSERT INTO public.orders (id, order_date, order_status, buyer_id) VALUES (6, '2024-11-22 12:12:38.688795', 'PENDING', 4);
+INSERT INTO public.orders (id, order_date, order_status, buyer_id) VALUES (7, '2024-11-22 12:13:16.222657', 'PENDING', 3);
